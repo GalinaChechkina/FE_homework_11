aeac8f87e2c5518ec7dab710a2a3a2acea9ce602
GalinaChechkina <galina.chechkina@gmail.com>
2024-01-22 17:36:02 +0100

    homework

---
+++ script/script.js
@@ -0,0 +1,34 @@
+// 1. Задача: "Удвоение зарплаты" У вас есть массив объектов, представляющих сотрудников 
+// с полями "имя" и "зарплата". Используя метод map, удвойте зарплату каждого сотрудника.
+const employees = [
+    { name: 'Alice', salary: 5000 },
+    { name: 'Bob', salary: 6000 },
+    { name: 'Charlie', salary: 4500 }
+  ];
+  
+const emplWithNewSalary = employees.map(e =>( {
+    name: e.name, 
+    salary: e.salary * 2
+}));
+console.log("1. Doubling salary:");
+emplWithNewSalary.forEach(e => console.log(`${e.name} - ${e.salary}`));
+
+// 2. Задача: "Фильтрация сотрудников" Используя метод filter, отфильтруйте сотрудников, 
+// у которых зарплата выше 5500.
+const employees_1 = employees.filter(e => e.salary > 5500);
+console.log("2. Employee filtering:");
+employees_1.forEach(e => console.log(`${e.name} - ${e.salary}`));
+
+// 3. Задача: "Общая зарплата" Используя метод reduce, вычислите общую зарплату 
+// всех сотрудников.
+const totalSalary = employees.reduce((acc, e) => acc + e.salary, 0);
+console.log(`3. Total salary is ${totalSalary}.`);
+
+// 4. Задача: "Имена в верхнем регистре" Используя метод map, преобразуйте имена всех 
+// сотрудников к верхнему регистру.
+const employees_2 = employees.map(e =>({
+    name: e.name.toUpperCase(), 
+    salary: e.salary
+}));
+console.log("4. Uppercase names:");
+employees_2.forEach(e => console.log(`${e.name} - ${e.salary}`));
